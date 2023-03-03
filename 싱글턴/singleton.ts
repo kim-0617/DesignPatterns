4282dad6df03bb5f5fff886616d77fd5d5738afd
kim-0617 <kkk5993@naver.com>
2023-03-04 01:03:03 +0900

    싱글턴 패턴 정복
    readme 추가

--- 싱글턴/singleton.ts
+++ 싱글턴/singleton.ts
@@ -1,33 +1,28 @@
 class Singleton {
+  // 실제로 싱글턴에 접근하는 객체들이 이용할 멤버변수, 이걸로 비즈니스 로직 실행
   private static instance: Singleton;
 
-  private constructor() {} // 생성자는 항상 private
+  // 외부에서 인스턴스 생성 금지
+  private constructor() {}
 
   public static getInstance(): Singleton {
-    // 싱글턴 객체 생성 메서드
+    // 없으면 싱글턴 클래스의 유일한 인스턴스를 생성한다.
     if (!Singleton.instance) {
       Singleton.instance = new Singleton();
     }
-
-    return Singleton.instance;
+    return Singleton.instance; // 있으면 있는거 return
   }
 
-  // 비즈니스 로직
-  public someBusinessLogic() {
-    // ...
-  }
-}
-
-function clientCode() {
-  const s1 = Singleton.getInstance();
-  const s2 = Singleton.getInstance();
-
-  // 싱글턴 패턴을 따르는 클래스의 getInstance 메서드를 통해 생성된 객체는 항상 동일한 객체를 공유하게 된다.
-  if (s1 === s2) {
-    console.log("Singleton works, both variables contain the same instance.");
-  } else {
-    console.log("Singleton failed, variables contain different instances.");
+  // 실제 수행할 비즈니스 로직
+  public doSomething(): void {
+    console.log("Singleton instance is doing something.");
   }
 }
 
-clientCode();
+// 2개를 생성하던 100개를 생성하던 싱글턴으로 만들어진 인스턴스는 모두 동일한 인스턴스이다.
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
+
+console.log(instance1 === instance2); // true
+instance1.doSomething(); // "Singleton instance is doing something."
+instance2.doSomething(); // "Singleton instance is doing something."
