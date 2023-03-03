789aa7884037b0a2c122d4d9b4c2bbf97be7b0d1
kim-0617 <kkk5993@naver.com>
2023-03-04 01:27:57 +0900

    빌더 패턴 정복
    예제 설명 추가

--- 빌더/builder.ts
+++ 빌더/builder.ts
@@ -1,141 +1,67 @@
-/**
- * The Builder interface specifies methods for creating the different parts of
- * the Product objects.
- */
-interface Builder {
-  producePartA(): void;
-  producePartB(): void;
-  producePartC(): void;
-}
+// 최종적으로 반환될 제품
+class Product {
+  private name: string; // 제품의 이름
+  private price: number; // 제품 가격
+  private description: string; // 제품 사용 설명서
 
-/**
- * The Concrete Builder classes follow the Builder interface and provide
- * specific implementations of the building steps. Your program may have several
- * variations of Builders, implemented differently.
- */
-class ConcreteBuilder1 implements Builder {
-  private product: Product1;
-
-  /**
-   * A fresh builder instance should contain a blank product object, which is
-   * used in further assembly.
-   */
-  constructor() {
-    this.reset();
+  // 생성자
+  constructor(name: string, price: number, description: string) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
   }
 
-  public reset(): void {
-    this.product = new Product1();
+  // getter 메서드들
+  public getName(): string {
+    return this.name;
   }
 
-  /**
-   * All production steps work with the same product instance.
-   */
-  public producePartA(): void {
-    this.product.parts.push("PartA1");
+  public getPrice(): number {
+    return this.price;
   }
 
-  public producePartB(): void {
-    this.product.parts.push("PartB1");
-  }
-
-  public producePartC(): void {
-    this.product.parts.push("PartC1");
-  }
-
-  /**
-   * Concrete Builders are supposed to provide their own methods for
-   * retrieving results. That's because various types of builders may create
-   * entirely different products that don't follow the same interface.
-   * Therefore, such methods cannot be declared in the base Builder interface
-   * (at least in a statically typed programming language).
-   *
-   * Usually, after returning the end result to the client, a builder instance
-   * is expected to be ready to start producing another product. That's why
-   * it's a usual practice to call the reset method at the end of the
-   * `getProduct` method body. However, this behavior is not mandatory, and
-   * you can make your builders wait for an explicit reset call from the
-   * client code before disposing of the previous result.
-   */
-  public getProduct(): Product1 {
-    const result = this.product;
-    this.reset();
-    return result;
+  public getDescription(): string {
+    return this.description;
   }
 }
 
-/**
- * It makes sense to use the Builder pattern only when your products are quite
- * complex and require extensive configuration.
- *
- * Unlike in other creational patterns, different concrete builders can produce
- * unrelated products. In other words, results of various builders may not
- * always follow the same interface.
- */
-class Product1 {
-  public parts: string[] = [];
+// 제품 빌더
+// 다양한 조건과 순서에 맞게, 입맞게 맞게 제품을 빌드할 수 있다.
+class ProductBuilder {
+  private name: string;
+  private price: number;
+  private description: string;
 
-  public listParts(): void {
-    console.log(`Product parts: ${this.parts.join(", ")}\n`);
+  // 이름 초기화 : 제품의 이름은 있어야겠죠?
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  // 모든 메서드들은 값을 할당 후 본인을 return 합니다. 그래야 메서드 체이닝 형식으로 제품을 빌드할 수 있기 때문입니다.
+
+  // 제품의 가격정의
+  public setPrice(price: number): ProductBuilder {
+    this.price = price;
+    return this;
+  }
+
+  // 제품 사용설명서
+  public setDescription(description: string): ProductBuilder {
+    this.description = description;
+    return this;
+  }
+
+  // 모든 과정이 끝났다면 최종적으로 build 메서드를 호출에서 제품객체 생성!
+  public build(): Product {
+    return new Product(this.name, this.price, this.description);
   }
 }
 
-/**
- * The Director is only responsible for executing the building steps in a
- * particular sequence. It is helpful when producing products according to a
- * specific order or configuration. Strictly speaking, the Director class is
- * optional, since the client can control builders directly.
- */
-class Director {
-  private builder: Builder;
+const product = new ProductBuilder("아이폰") // this.name = "아이폰"
+  .setPrice(1500000) // this.price = 1500000
+  .setDescription("Apple 사의 스마트폰입니다.") // this.description = "Apple 사의 스마트폰입니다."
+  .build(); // 라는 제품을 만들었습니다!
 
-  /**
-   * The Director works with any builder instance that the client code passes
-   * to it. This way, the client code may alter the final type of the newly
-   * assembled product.
-   */
-  public setBuilder(builder: Builder): void {
-    this.builder = builder;
-  }
-
-  /**
-   * The Director can construct several product variations using the same
-   * building steps.
-   */
-  public buildMinimalViableProduct(): void {
-    this.builder.producePartA();
-  }
-
-  public buildFullFeaturedProduct(): void {
-    this.builder.producePartA();
-    this.builder.producePartB();
-    this.builder.producePartC();
-  }
-}
-
-/**
- * The client code creates a builder object, passes it to the director and then
- * initiates the construction process. The end result is retrieved from the
- * builder object.
- */
-function clientCode(director: Director) {
-  const builder = new ConcreteBuilder1();
-  director.setBuilder(builder);
-
-  console.log("Standard basic product:");
-  director.buildMinimalViableProduct();
-  builder.getProduct().listParts();
-
-  console.log("Standard full featured product:");
-  director.buildFullFeaturedProduct();
-  builder.getProduct().listParts();
-
-  // Remember, the Builder pattern can be used without a Director class.
-  console.log("Custom product:");
-  builder.producePartA();
-  builder.producePartC();
-  builder.getProduct().listParts();
-}
-
-const director = new Director();
-clientCode(director);
+console.log(product.getName()); // "아이폰"
+console.log(product.getPrice()); // 1500000
+console.log(product.getDescription()); // "Apple 사의 스마트폰입니다."
