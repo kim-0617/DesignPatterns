c50873d1e66fe228abb7e4221ec0c0ce928d8ef2
JAM-PARK <chwk05@gmail.com>
2023-04-09 16:36:24 +0900

    💡 상태 예제 주석 추가

--- 행동 패턴/상태/state.ts
+++ 행동 패턴/상태/state.ts
@@ -1,7 +1,12 @@
-// @type 은 바로 밑의변수 state가 State 타입이란 걸 알려줍니다.
+/**
+ * Context는 클라이언트에게 필요한 인터페이스를 정의합니다.
+ * 또한 현재 Context의 상태를 나타내는 State 하위 클래스의 인스턴스에 대한 참조를 유지합니다.
+ */
 class Context {
   /**
    * @type {State} A reference to the current state of the Context.
+   * @type {State} Context의 현재 상태를 나타내는 참조입니다.
+   * (SH) @type 은 바로 밑의변수 state가 State 타입이란 걸 알려줍니다.
    */
   private state: State;
 
@@ -9,12 +14,18 @@ class Context {
     this.transitionTo(state);
   }
 
+  /**
+   * Context는 런타임에 State 객체를 변경할 수 있습니다.
+   */
   public transitionTo(state: State): void {
     console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
     this.state = state;
     this.state.setContext(this);
   }
 
+  /**
+   * Context는 일부 동작을 현재 State 객체에 위임합니다.
+   */
   public request1(): void {
     this.state.handle1();
   }
@@ -24,6 +35,11 @@ class Context {
   }
 }
 
+/**
+ * 기본 State 클래스는 모든 구체적인 State에서 구현해야 할 메서드를 선언하고,
+ * 해당 State와 연관된 Context 객체에 대한 역참조를 제공합니다.
+ * 이 역참조는 State가 Context를 다른 State로 전이하기 위해 사용될 수 있습니다.
+ */
 abstract class State {
   protected context: Context;
 
@@ -36,6 +52,9 @@ abstract class State {
   public abstract handle2(): void;
 }
 
+/**
+ * 구상 State는 Context의 상태와 관련된 다양한 동작을 구현합니다.
+ */
 class ConcreteStateA extends State {
   public handle1(): void {
     console.log("ConcreteStateA handles request1.");
@@ -60,6 +79,9 @@ class ConcreteStateB extends State {
   }
 }
 
+/**
+ * 클라이언트 코드입니다.
+ */
 const context = new Context(new ConcreteStateA());
 context.request1();
 context.request2();
